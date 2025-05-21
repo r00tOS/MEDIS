da71b828dee7d58f807fdcb5d480bfc808dd7d5f
Martin Brucker <martin.brucker@posteo.de>
2025-05-21 13:43:40 +0200

    moved code into own file

--- scripts/render_trupps.js
+++ scripts/render_trupps.js
@@ -260,3 +260,8 @@ function renderTrupps() {
     });
   });
 }
+
+function formatTime(ms) {
+  const d = new Date(ms);
+  return d.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+}
